39a920a1b9652e0366dd8dae66206d7eca75c28b
SHarif <mondal.sharif@gmail.com>
2025-07-09 11:12:21 +0530

    hgff

--- terratest/test1_test.go
+++ terratest/test1_test.go
@@ -2,18 +2,9 @@ package test
 
 import (
 	"testing"
+	
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/assert"
 )
-
-// Test initializing and applying the Terraform configuration
-func TestTerraformInitApply(t *testing.T) {
-	terraformOptions := &terraform.Options{
-		TerraformBinary: "terraform",
-		TerraformDir:    "../vmtest/",
-	}
-	terraform.InitAndApply(t, terraformOptions)
-}
 
 // Test destroying the Terraform resources after apply
 func TestTerraformDestroy(t *testing.T) {
